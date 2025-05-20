bea09144008e4350a5a31e8e2bb37b63f1b292f9
ashbal18ts <ashbalfc99@gmail.com>
2025-05-21 01:55:16 +0600

    baru

--- src/app/portofolio/page.tsx
+++ src/app/portofolio/page.tsx
@@ -26,7 +26,7 @@ export default function Portfolio() {
               Platform E-commerce XYZ
             </h2>
             <p>
-              <strong>Teknologi:</strong> React, Node.js, MongoDB, AWS
+              <strong>Teknologi:</strong> React, Node.js, Prisma Express, AWS
             </p>
             <p>
               <strong>Situasi:</strong> XYZ Retail ingin memperluas bisnisnya ke
@@ -35,6 +35,14 @@ export default function Portfolio() {
             <p>
               <strong>Hasil:</strong> Peningkatan penjualan 35% dalam 3 bulan.
             </p>
+            <a
+              href="https://company1-git-main-adams-projects-0257165f.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mt-2 text-blue-600 hover:underline text-center"
+            >
+              Lihat Proyek
+            </a>
           </div>
         </motion.div>
 
@@ -56,7 +64,7 @@ export default function Portfolio() {
               Website Portofolio
             </h2>
             <p>
-              <strong>Teknologi:</strong> Next.js, Tailwind CSS, Vercel
+              <strong>Teknologi:</strong> React, Next.Js, Tailwind CSS, Express Prisma
             </p>
             <p>
               <strong>Situasi:</strong> Membangun website portofolio pribadi.
@@ -64,6 +72,14 @@ export default function Portfolio() {
             <p>
               <strong>Hasil:</strong> Website responsif & menarik.
             </p>
+             <a
+              href="https://adam-xi.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mt-2 text-blue-600 hover:underline text-center"
+            >
+              Lihat Proyek
+            </a>
           </div>
         </motion.div>
 
@@ -85,7 +101,7 @@ export default function Portfolio() {
               Webiste Blogs
             </h2>
             <p>
-              <strong>Teknologi:</strong> React, Chart.js, Firebase
+              <strong>Teknologi:</strong> React, Next.Js, Tailwind CSS, Express Prisma
             </p>
             <p>
               <strong>Situasi:</strong> Perusahaan butuh dashboard analytics.
@@ -93,9 +109,54 @@ export default function Portfolio() {
             <p>
               <strong>Hasil:</strong> Data real-time untuk keputusan lebih baik.
             </p>
+             <a
+              href="https://blogs-me.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mt-2 text-blue-600 hover:underline text-center"
+            >
+              Lihat Proyek
+            </a>
           </div>
         </motion.div>
       </div>
-    </div>
+
+              {/* Proyek 4 */}
+        <motion.div
+          whileHover={{ scale: 1.05 }}
+          initial={{ scale: 0.85 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="w-full"
+        >
+          <div className="p-2 sm:p-4 bg-white shadow-lg rounded-xl border border-gray-200 flex flex-col text-xs sm:text-sm">
+            <img
+              src="/concerthub.png"
+              alt="Dashboard"
+              className="w-full h-20 sm:h-40 object-cover rounded-lg mb-2 sm:mb-4"
+            />
+            <h2 className="text-lg sm:text-xl font-semibold mb-1 sm:mb-2 text-center sm:text-left">
+              Webiste Tiket Konser Musik
+            </h2>
+            <p>
+              <strong>Teknologi:</strong> React, Next.Js, Tailwind CSS, Express Prisma
+            </p>
+            <p>
+              <strong>Situasi:</strong> Jual Beli Tiket Konser Musik.
+            </p>
+            <p>
+              <strong>Hasil:</strong> Data real-time untuk keputusan lebih baik.
+            </p>
+             <a
+              href="https://concerthubs.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mt-2 text-blue-600 hover:underline text-center"
+            >
+              Lihat Proyek
+            </a>
+          </div>
+        </motion.div>
+      </div>
   );
 }
